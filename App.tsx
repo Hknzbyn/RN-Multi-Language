d1bf27f8d5908c7d8af60726e7318a8b7e870a8c
hknzbyn <hakanozbeyen@gmail.com>
2023-04-07 23:30:21 +0300

    instructions added

--- App.tsx
+++ App.tsx
@@ -1,9 +1,17 @@
-import { useEffect } from 'react';
-import { StyleSheet, Text } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Alert,
+  Pressable,
+  Platform,
+  NativeModules,
+} from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { I18nextProvider, useTranslation } from 'react-i18next';
 import i18n from './i18n';
 import { LinearGradient } from 'expo-linear-gradient';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function App() {
   return (
@@ -17,19 +25,60 @@ export default function App() {
 
 const Main = () => {
   const { t, i18n } = useTranslation('global');
+  const usableLanguages = ['en', 'es', 'tr'];
 
-  useEffect(() => {
-    return () => {};
-  }, []);
+  //Change language & save AsyncStorage function - show Alert
+  const changeLanguage = (lng: string) => {
+    i18n.changeLanguage(lng);
+    AsyncStorage.setItem('language', lng);
+
+    Alert.alert(
+      t(`languages.${lng}`),
+      t('changeLang.message'),
+      [{ text: 'OK', onPress: () => console.log('OK') }],
+      { cancelable: false }
+    );
+  };
 
   const chosenLanguage = i18n.language;
-  console.log('chosenLanguage---', chosenLanguage);
+
+  const Buttons = () => {
+    return (
+      <View style={{ flexDirection: 'row', marginTop: 50 }}>
+        {usableLanguages.map((lng) => {
+          return (
+            <Pressable
+              key={lng}
+              style={{
+                backgroundColor: '#123456',
+                padding: 8,
+                borderRadius: 5,
+                marginHorizontal: 15,
+              }}
+              onPress={() => changeLanguage(lng)}>
+              <Text style={{ color: '#fff', fontSize: 18 }}>
+                {t(`languages.${lng}`)}
+              </Text>
+            </Pressable>
+          );
+        })}
+      </View>
+    );
+  };
 
   return (
     <LinearGradient
       colors={['#4c669f', '#3b5998', '#192f6a']}
       style={styles.container}>
+      <Text style={styles.text}>
+        {t('home.currentLang')}{' '}
+        <Text style={{ textDecorationLine: 'underline' }}>
+          {t(`languages.${chosenLanguage}`)}
+        </Text>
+      </Text>
       <Text style={styles.text}>{t('home.message')}</Text>
+
+      <Buttons />
     </LinearGradient>
   );
 };
@@ -39,10 +88,10 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'flex-start',
+    justifyContent: 'center',
   },
   text: {
-    marginTop: 75,
+    margin: 10,
     color: '#fff',
     fontSize: 24,
     fontWeight: 'bold',
--- i18n.ts
+++ i18n.ts
@@ -4,29 +4,50 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { GlobalEN, GlobalTR, GlobalES } from './src/translations/';
 import { getLocales } from 'expo-localization';
 
-import { NativeModules, Platform } from 'react-native';
-
 export const locales = {
   en: GlobalEN,
   es: GlobalES,
   tr: GlobalTR,
 };
 
-export const DEFAULT_LOCALE = 'tr'; // DEfault language
+// -HOW IS WORKING-
+// 1. If user has saved language, use it
+// 2. If user has not saved language, use device language
+// 3. If device language is not supported, use default language
 
+// -NOTES-
+
+// 1. If you do not want to use the saved language
+//  1a. Extract parts containing AsyncStorage from changeLanguage function in app.tsx.
+//  1b. Rearrange the detect partition from useLanguageStorage
+// 2. If the registered language and device language are not available, change the DEFAULT_LOCALE value.
+// 3. If you don't want to use device language, remove step 2 and edit fallbackLng: DEFAULT_LOCALE.
+// 4. If you don't use expo-localization when finding defaultLanguage, use alternative step 2.
+
+// Alternative Step 2 (import { Platform, NativeModules } from 'react-native';)
+//  const deviceLanguage =
+//    Platform.OS === 'ios'
+//      ? NativeModules.SettingsManager.settings.AppleLocale ||
+//        NativeModules.SettingsManager.settings.AppleLanguages[0] //iOS 13
+//      : NativeModules.I18nManager.localeIdentifier;
+
+// Step 3
+export const DEFAULT_LOCALE = 'tr';
+
+// Step 2
 export const defaultLanguage = getLocales()[0].languageCode || DEFAULT_LOCALE;
 
 const useLanguageStorage: LanguageDetectorAsyncModule = {
   type: 'languageDetector',
   async: true,
   detect: async (callback) => {
+    //Step 1
     AsyncStorage.getItem('language').then((lang) => {
       if (lang) return callback(lang);
     });
   },
   init: () => null,
   cacheUserLanguage: async (language: string) => {
-    console.log('language', language);
     AsyncStorage.setItem('language', language);
   },
 };
@@ -35,7 +56,7 @@ i18n
   .use(useLanguageStorage)
   .use(initReactI18next)
   .init({
-    fallbackLng: DEFAULT_LOCALE,
+    fallbackLng: defaultLanguage,
     compatibilityJSON: 'v3',
 
     resources: locales,
